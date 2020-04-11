4bc66d297face89c6b060db9034242a0eacdf7e8
venuvis <vishnu.venugopal@bakerhughes.com>
2020-04-11 10:24:35 +0530

    changesvishnu

---
+++ src/test/java/Test2.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+
+    @Test(groups = "smoke")
+    public void Test2_Test1(){
+
+        System.out.println(this.getClass());
+
+
+    }
+    @Test(groups = "smoke")
+    public void Test2_Test2(){
+
+        System.out.println(this.getClass());
+
+
+    }
+    @Test(groups = "smoke")
+    public void Test2_Test3(){
+
+        System.out.println(this.getClass());
+
+
+    }
+
+
+
+}
---
+++ src/test/java/Test3.java
@@ -0,0 +1,30 @@
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+
+    @Test(groups = "sanity")
+    public void Test3_Test1(){
+
+        System.out.println(this.getClass());
+
+
+    }
+    @Test(groups = "sanity")
+    public void Test3_Test2(){
+
+        System.out.println(this.getClass());
+
+
+    }
+    @Test(groups = "sanity")
+    public void Test3_Test3(){
+
+        System.out.println(this.getClass());
+
+
+    }
+
+
+
+}

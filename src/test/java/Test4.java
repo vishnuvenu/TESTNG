81c3b363c46ff79b7f6892117ef16b5bea177e50
venuvis <vishnu.venugopal@bakerhughes.com>
2020-04-11 10:44:43 +0530

    changes

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -1,23 +1,23 @@
 import org.testng.annotations.Test;
 
-public class Test3 {
+public class Test4 {
 
 
-    @Test(groups = "sanity")
+    @Test(groups = "smoke")
     public void Test3_Test1(){
 
         System.out.println(this.getClass());
 
 
     }
-    @Test(groups = "sanity")
+    @Test(groups = "smoke")
     public void Test3_Test2(){
 
         System.out.println(this.getClass());
 
 
     }
-    @Test(groups = "sanity")
+    @Test(groups = "smoke")
     public void Test3_Test3(){
 
         System.out.println(this.getClass());
